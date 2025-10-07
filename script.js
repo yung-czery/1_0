6beb431b297e5abbd8f3be62cee14d520e45e543
yung-czery <165099503+yung-czery@users.noreply.github.com>
2025-10-07 07:46:00 +0000

    1_1.1

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 (function () {
-  //TODO
+  const button = document.getElementById('ex1_button');
+  const content = document.getElementById('ex1_content')
+
+  const numbers = Array.from({ length: 10 }, (_, i) => i);
+
+  button.addEventListener('click', () => {
+    content.innerHTML = numbers;
+  })
 })();
